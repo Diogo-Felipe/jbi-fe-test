78770d6fd74567db0023da49f665d84ae9ba78b4
Diogo-Felipe <dlcfelipp@gmail.com>
2024-12-28 01:31:35 -0300

    data mock

---
+++ src/__mock__/dataModel.js
@@ -0,0 +1,37 @@
+export const Library = {
+  Components: [
+    {
+      Name: 'Free Form Container',
+      Categories: ['Layout'],
+    },
+    {
+      Name: 'Flex Container',
+      Categories: ['Layout'],
+    },
+    {
+      Name: 'Bar Chart',
+      Categories: ['Charts'],
+    },
+    {
+      Name: 'Line Chart',
+      Categories: ['Charts'],
+    },
+    {
+      Name: 'Button',
+      Categories: ['Controls'],
+    },
+    {
+      Name: 'Input Field',
+      Categories: ['Controls', 'Inputs'],
+    },
+    {
+      Name: 'Checkbox',
+      Categories: ['Controls', 'Inputs'],
+    },
+    {
+      Name: 'Radio Button',
+      Categories: ['Controls', 'Inputs'],
+    },
+  ],
+  Categories: ['Controls', 'Inputs', 'Layout', 'Custom', 'Charts'],
+};
